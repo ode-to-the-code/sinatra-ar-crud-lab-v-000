d117ab2e2759b4fe261ad008442ba0fdf4796a89
Jad Abdallah <justcallmethis@gmail.com>
2018-12-30 16:51:26 +0000

    Automatically backed up by Learn

--- db/migrate/20181230164943_create_articles.rb
+++ db/migrate/20181230164943_create_articles.rb
@@ -1,6 +1,12 @@
 class CreateArticles < ActiveRecord::Migration[5.1]
   def change
+    create_table :articles do |t|
     t.string :title 
     t.string :content
+  end 
   end
 end
+
+    create_table :users do |t|
+      t.string :name
+    end
