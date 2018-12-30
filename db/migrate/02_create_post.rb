53e5fa9245fdf2eb8e4ec567075a312c1f3198f5
Jad Abdallah <justcallmethis@gmail.com>
2018-12-30 16:44:35 +0000

    Automatically backed up by Learn

--- db/migrate/02_create_post.rb
+++
@@ -1,9 +0,0 @@
-class CreatePost < ActiveRecord::Migration[5.1]
-  def change
-    create_table :posts do |t|
-      t.string :name
-      t.string :ingredients
-      t.integer :cook_time
-    end
-  end
-end
